73c2be35a74bab94ca2c1eece55dc5f1b2d6e311
Stephen Jacobs <stephenjay1187@gmail.com>
2019-01-10 16:35:23 -0800

    Updated styles and added search bar

--- client/src/components/navigation/navigation.js
+++ client/src/components/navigation/navigation.js
@@ -1,63 +1,110 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-// import InputBase from '@material-ui/core/InputBase';
+import InputBase from '@material-ui/core/InputBase';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-// import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from '@material-ui/icons/Search';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import homeIcon from '../../svg/navigation/newspaper.svg';
 import './navigation.css';
-import homeIcon from './newspaper.svg'
-//Components
-// import Filter from '../../components/filter/filter'
 
-const styles = {
+const styles = theme => ({
   root: {
-    flexGrow: 1
+    width: '100%',
   },
   grow: {
     flexGrow: 1,
   },
   menuButton: {
     marginLeft: -12,
-    marginRight: 20,
+    //marginRight: 20,
   },
-};
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit,
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
+  },
+});
 
 class Navigation extends Component {
   render() {
-    // const classes = styles;
+    const { classes } = this.props;
     return (
-        // <nav className="navbar navbar-light bg-light justify-content-between top-fixed toolbar">
-        //   <a className="navbar-brand" href="/"><i className="fas fa-newspaper"></i>&nbsp;React News Parser</a>
-        //   <Filter onFilterTextChange={this.props.onFilterTextChange}/>
-        // </nav>     
-        <div className="root">
-          <AppBar position="static">
+        <div>
+           <AppBar position="static">
             <Toolbar>
-              <IconButton className="menuButton" color="inherit" aria-label="Menu">
+              <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
                 <MenuIcon />
               </IconButton>
-              <Typography variant="h6" color="inherit" className="grow">
-              <img src={homeIcon} height="50" width="50" alt="news"/>
-              <span className="logo-title">grapevine</span>
+              <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+              <img src={homeIcon} className="header-logo" height="50" width="50" alt="news"/>
+               <span className="logo-title">grapevine</span>
               </Typography>
-              {/* <InputBase placeholder="Search…" /> */}
-              <div className="grow" />
-              {/* <div className="search">
-                <div className="searchIcon">
+              <div className={classes.grow} />
+              <div className={classes.search}>
+                <div className={classes.searchIcon}>
                   <SearchIcon />
                 </div>
-                <InputBase placeholder="Search…" className="inputRoot inputInput"/>
-              </div> */}
+                <InputBase
+                  placeholder="Search…"
+                  classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                  }}
+                />
+              </div>
             </Toolbar>
-          </AppBar>
-        </div>
+            </AppBar>
+         </div>  
       )
   }
-}
+};
 
 Navigation.propTypes = {
   classes: PropTypes.object.isRequired,
