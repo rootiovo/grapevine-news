a43e648cb43067dd4676191a1640565311247b15
Stephen Jacobs <stephenjay1187@gmail.com>
2019-01-14 14:51:46 -0800

    Put weather icon hash into separate file

---
+++ client/src/components/weather/icon-map.js
@@ -0,0 +1,17 @@
+import cloudy from '../../svg/weather/cloudy.svg'
+import cloudyNight from '../../svg/weather/cloudy-night-.svg'
+import rain from '../../svg/weather/rain.svg'
+import sun from '../../svg/weather/sun.svg'
+import moon from '../../svg/weather/crescent-moon.svg'
+ 
+ //Map icons to data
+ const iconHash = new Map([
+    ['clear-day', sun],
+    ['partly-cloudy-day', cloudy],
+    ['rain', rain],
+    ['partly-cloudy-night', cloudyNight],
+    ['clear-night', moon],
+    ['cloudy', cloudy]
+  ]);
+
+  export default iconHash;
