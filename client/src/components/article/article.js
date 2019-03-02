84e80c9cef982e1fb63c310177a457dfc252ab4c
Stephen Jacobs <stephenjay1187@gmail.com>
2019-03-01 16:14:58 -0800

    Added share button.

--- client/src/components/article/article.js
+++ client/src/components/article/article.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-// import CardActions from '@material-ui/core/CardActions';
-// import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-// import ShareIcon from '@material-ui/icons/Share';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import moment from 'moment';
-import './article.css';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import ShareIcon from "@material-ui/icons/Share";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import moment from "moment";
+import "./article.css";
 
 const styles = theme => ({
   card: {
     // maxWidth: 750,
-    marginBottom: '25px;',
+    marginBottom: "25px;"
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
+    paddingTop: "56.25%" // 16:9
   },
   actions: {
-    display: 'flex',
-  },
+    display: "flex"
+  }
 });
 
 function Article(props) {
@@ -31,27 +31,37 @@ function Article(props) {
 
   return (
     <Card className={classes.card}>
-      <a href={article.url} target='_blank' rel='noopener noreferrer'>
+      <a href={article.url} target="_blank" rel="noopener noreferrer">
         <CardActionArea>
           <CardMedia
             className={classes.media}
             image={article.urlToImage}
             title={article.title}
-            alt='/'
+            alt="/"
           />
           <CardContent>
-            <Typography gutterBottom variant='h5' component='h2'>
+            <Typography gutterBottom variant="h5" component="h2">
               {article.title}
             </Typography>
-            <Typography component='p' className='card-text'>
+            <Typography component="p" className="card-text">
               {article.description}
             </Typography>
-            <Typography component='p' className='publish-date'>
-              {`${article.source.name} · ${moment(article.publishedAt).fromNow()}`}
+            <Typography component="p" className="publish-date">
+              {`${article.source.name} · ${moment(
+                article.publishedAt
+              ).fromNow()}`}
             </Typography>
           </CardContent>
         </CardActionArea>
       </a>
+      <CardActions
+        className={`article-actions ${classes.actions}`}
+        disableActionSpacing
+      >
+        <IconButton aria-label="Share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
     </Card>
   );
 }
