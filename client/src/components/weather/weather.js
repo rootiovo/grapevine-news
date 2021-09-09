1309944af00f9193a58b5e03d37c703e03fbfaff
Stephen Jacobs <stephenjay1187@protonmail.com>
2021-09-09 16:00:01 -0700

    Cleaned up linting errors in the weather component and removed deprecated componentWillMount lifecycle method.

--- client/src/components/weather/weather.js
+++ client/src/components/weather/weather.js
@@ -6,18 +6,14 @@ import iconHash from './icon-map';
 import './weather.css';
 
 class Weather extends Component {
-  
-  constructor(props){
-
+  constructor(props) {
     super(props);
     this.state = {
-      weather: null
+      weather: null,
     };
+  }
 
-
-  };
-
-  UNSAFE_componentWillMount() {
+  componentDidMount() {
     // get weather data for current geolocation
     navigator.geolocation.getCurrentPosition((position) => {
       const coords = position;
@@ -42,9 +38,11 @@ class Weather extends Component {
     return (
       <React.Fragment>
         <Paper elevation={1} className='weather'>
-          {this.state.weather &&
+          {this.state.weather && (
             <div className='container-weather'>
-              <div className='weather-header'>{this.state.weather.timezone.replace('_', ' ').split('/')[1]}</div>
+              <div className='weather-header'>
+                {this.state.weather.timezone.replace('_', ' ').split('/')[1]}
+              </div>
               <hr />
               <div className='container-current'>
                 <div className='column-current' style={{ marginRight: '40%' }}>
@@ -60,7 +58,9 @@ class Weather extends Component {
                   <div className='current-temp'>
                     {Math.round(this.state.weather.currently.temperature)}°
                   </div>
-                  <div className='current-status'>{this.state.weather.currently.summary}</div>
+                  <div className='current-status'>
+                    {this.state.weather.currently.summary}
+                  </div>
                 </div>
               </div>
               <hr />
@@ -73,40 +73,119 @@ class Weather extends Component {
                     width='50'
                     alt='/'
                   />
-                  <p className='forecast-high'>{Math.round(this.state.weather.daily.data[0].temperatureHigh)}°</p>
-                  <p className='forecast-low'>{Math.round(this.state.weather.daily.data[0].temperatureLow)}°</p>
+                  <p className='forecast-high'>
+                    {Math.round(
+                      this.state.weather.daily.data[0].temperatureHigh,
+                    )}
+                    °
+                  </p>
+                  <p className='forecast-low'>
+                    {Math.round(
+                      this.state.weather.daily.data[0].temperatureLow,
+                    )}
+                    °
+                  </p>
                 </div>
                 <div className='column-forecast'>
-                  <div className='forecast-day'>{moment().add(1, 'days').format('ddd')}</div>
-                  <img src={iconHash.get(this.state.weather.daily.data[1].icon)} height='50' width='50' alt='/' />
-                  <p className='forecast-high'>{Math.round(this.state.weather.daily.data[1].temperatureHigh)}°</p>
-                  <p className='forecast-low'>{Math.round(this.state.weather.daily.data[1].temperatureLow)}°</p>
+                  <div className='forecast-day'>
+                    {moment().add(1, 'days').format('ddd')}
+                  </div>
+                  <img
+                    src={iconHash.get(this.state.weather.daily.data[1].icon)}
+                    height='50'
+                    width='50'
+                    alt='/'
+                  />
+                  <p className='forecast-high'>
+                    {Math.round(
+                      this.state.weather.daily.data[1].temperatureHigh,
+                    )}
+                    °
+                  </p>
+                  <p className='forecast-low'>
+                    {Math.round(
+                      this.state.weather.daily.data[1].temperatureLow,
+                    )}
+                    °
+                  </p>
                 </div>
                 <div className='column-forecast'>
-                  <div className='forecast-day'>{moment().add(2, 'days').format('ddd')}</div>
-                  <img src={iconHash.get(this.state.weather.daily.data[2].icon)} height='50' width='50' alt='/' />
-                  <p className='forecast-high'>{Math.round(this.state.weather.daily.data[2].temperatureHigh)}°</p>
-                  <p className='forecast-low'>{Math.round(this.state.weather.daily.data[2].temperatureLow)}°</p>
+                  <div className='forecast-day'>
+                    {moment().add(2, 'days').format('ddd')}
+                  </div>
+                  <img
+                    src={iconHash.get(this.state.weather.daily.data[2].icon)}
+                    height='50'
+                    width='50'
+                    alt='/'
+                  />
+                  <p className='forecast-high'>
+                    {Math.round(
+                      this.state.weather.daily.data[2].temperatureHigh,
+                    )}
+                    °
+                  </p>
+                  <p className='forecast-low'>
+                    {Math.round(
+                      this.state.weather.daily.data[2].temperatureLow,
+                    )}
+                    °
+                  </p>
                 </div>
                 <div className='column-forecast'>
-                  <div className='forecast-day'>{moment().add(3, 'days').format('ddd')}</div>
-                  <img src={iconHash.get(this.state.weather.daily.data[3].icon)} height='50' width='50' alt='/' />
-                  <p className='forecast-high'>{Math.round(this.state.weather.daily.data[3].temperatureHigh)}°</p>
-                  <p className='forecast-low'>{Math.round(this.state.weather.daily.data[3].temperatureLow)}°</p>
+                  <div className='forecast-day'>
+                    {moment().add(3, 'days').format('ddd')}
+                  </div>
+                  <img
+                    src={iconHash.get(this.state.weather.daily.data[3].icon)}
+                    height='50'
+                    width='50'
+                    alt='/'
+                  />
+                  <p className='forecast-high'>
+                    {Math.round(
+                      this.state.weather.daily.data[3].temperatureHigh,
+                    )}
+                    °
+                  </p>
+                  <p className='forecast-low'>
+                    {Math.round(
+                      this.state.weather.daily.data[3].temperatureLow,
+                    )}
+                    °
+                  </p>
                 </div>
                 <div className='column-forecast'>
-                  <div className='forecast-day'>{moment().add(4, 'days').format('ddd')}</div>
-                  <img src={iconHash.get(this.state.weather.daily.data[4].icon)} height='50' width='50' alt='/' />
-                  <p className='forecast-high'>{Math.round(this.state.weather.daily.data[4].temperatureHigh)}°</p>
-                  <p className='forecast-low'>{Math.round(this.state.weather.daily.data[4].temperatureLow)}°</p>
+                  <div className='forecast-day'>
+                    {moment().add(4, 'days').format('ddd')}
+                  </div>
+                  <img
+                    src={iconHash.get(this.state.weather.daily.data[4].icon)}
+                    height='50'
+                    width='50'
+                    alt='/'
+                  />
+                  <p className='forecast-high'>
+                    {Math.round(
+                      this.state.weather.daily.data[4].temperatureHigh,
+                    )}
+                    °
+                  </p>
+                  <p className='forecast-low'>
+                    {Math.round(
+                      this.state.weather.daily.data[4].temperatureLow,
+                    )}
+                    °
+                  </p>
                 </div>
               </div>
               <div className='weather-summary'>
-                <i className='fas fa-info-circle' />&nbsp;
+                <i className='fas fa-info-circle' />
+                &nbsp;
                 {this.state.weather.daily.summary}
               </div>
             </div>
-          }
+          )}
         </Paper>
       </React.Fragment>
     );
